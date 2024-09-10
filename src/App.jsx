fae9262bc135529cd33776988b1799d603490c1f
Rodrigo_Gonçalves <rodrigoncalves.net@outlook.com>
2024-09-09 21:28:43 -0300

    inicio pagina login

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Home from './Components/Home';
@@ -12,8 +12,8 @@ const App = () => {
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/" elemnte={<Home />} />
-          <Route path="/login" element={<Login />} />
+          <Route path={'/'} element={<Home />} />
+          <Route path={'/login/*'} element={<Login />} />
         </Routes>
         <Footer />
       </BrowserRouter>
